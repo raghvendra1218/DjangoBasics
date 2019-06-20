7390ad2dc8c94609549a6c7bafdb70b945ca1476
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-06-20 00:53:25 -0700

    Add Models, populate sqllite

--- first_project/first_app/admin.py
+++ first_project/first_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import AccessRecord, Topic, Webpage
 
 # Register your models here.
+class TopicAdmin(admin.ModelAdmin):
+	list_display = ('name')
+
+
+class WebpageAdmin(admin.ModelAdmin):
+	list_display= ('topic', 'name', 'url')
+
+
+class AccessRecordAdmin(admin.ModelAdmin):
+	list_display = ('name', 'date')
+
+
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
