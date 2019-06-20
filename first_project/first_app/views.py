bc26191e43bed9ddccad89eb0dbb879d8c520e78
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-06-20 15:48:43 -0700

    Add First template and view

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic, AccessRecord, Webpage
 
 
 def index(request):
-	my_dict = {'insert_me': "Hello, Now I am coming from first_app/index.html"}
-	return render(request, 'first_app/index.html', context=my_dict)
+	webpages = AccessRecord.objects.order_by('date')
+	my_access_records = {'access_records':webpages}
+	# my_dict = {'insert_me': "Hello, Now I am coming from first_app/index.html"}
+	return render(request, 'first_app/index.html', context=my_access_records)
