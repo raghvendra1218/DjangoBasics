b3694774c1b37c38fd8889cd815fd2ff15c9416a
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-06-24 00:47:45 -0700

    Add Registration app

--- learning_users/basic_app/admin.py
+++ learning_users/basic_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from basic_app.models import UserProfileInfo
 
 # Register your models here.
+# class UserProfileInfoAdmin(admin.ModelAdmin):
+# 	list_display = ['username', 'email', 'password', 'portfolio_site', 'profile_pic']
+
+
+admin.site.register(UserProfileInfo)
+
+
