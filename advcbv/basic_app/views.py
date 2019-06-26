aeefec06db02a6a67aa50f4ed9413533c097a61f
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-06-25 19:40:18 -0700

    Add ListView and DetailView

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from . import models
+from django.views.generic import TemplateView, ListView, DetailView
 
 # Create your views here.
 class IndexView(TemplateView):
 	template_name = 'index.html'
 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		context["injected_content"] = 'Basic Injection' 
-		return context
+
+class SchoolListView(ListView):
+	# By default context name for the returned dict is modelname all lower suffixed by _list, in this case
+	# it will be school_list, we can override the name as below, now it will be dict schools
+	context_object_name = 'schools'
+	model = models.School
+
+
+class SchoolDetailView(DetailView):
+	# By default context name for the returned dict is modelname all lower case
+	# it will be school, we can override the name as below, now it will be dict school_detail
+	context_object_name = 'school_detail'
+	model = models.School
+	template_name =  'basic_app/school_detail.html'
 	
