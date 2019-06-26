8cedf7f5c70ba518952dca6115a52767c0c2c5e4
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-06-25 17:28:23 -0700

    Add Index View with injected content

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-class CBView(View):
-	def get(self, request):
-		return HttpResponse('Class based views are cool!')
+class IndexView(TemplateView):
+	template_name = 'index.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["injected_content"] = 'Basic Injection' 
+		return context
+	
